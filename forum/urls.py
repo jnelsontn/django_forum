52de1bd03d8552ca666ff7596024e3380d64be22
Jordan Nelson <jnelsontn@gmail.com>
2017-09-12 18:27:47 -0500

    Ability to edit/delete posts

--- forum/urls.py
+++ forum/urls.py
@@ -5,8 +5,11 @@ app_name = 'forum'
 urlpatterns = [
     url(r'^create/$', views.ForumCreateThreadView, name='create_thread'),
     url(r'^profile/(?P<pk>[0-9]+)/$', views.UserProfileView.as_view(), name='view_user_profile'),
+    url(r'^editprofile/$', views.UserProfileEditView.as_view(), name='view_edit_profile'),
     url(r'^$', views.ForumIndexView.as_view(), name='view_forum_index'),
     url(r'^(?P<slug>[-\w]*)/$', views.ForumThreadView.as_view(), name='view_thread'),
     url(r'^(?P<slug>[-\w]*)/reply/$', views.ForumCreateReplyView.as_view(), name='reply_thread'),
     url(r'^(?P<thread>[-\w]*)/(?P<slug>[-\w]*)/$', views.ForumPostView.as_view(), name='view_post'),
+    url(r'^(?P<thread>[-\w]*)/(?P<slug>[-\w]*)/edit/$', views.ForumPostEditView.as_view(), name='edit_post'),
+    url(r'^(?P<thread>[-\w]*)/(?P<slug>[-\w]*)/delete/$', views.ForumPostDeleteView.as_view(), name='delete_post'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import get_object_or_404, render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.views import generic
@@ -28,13 +28,6 @@ class ForumPostView(generic.DetailView):
 	template_name = 'post_detail.html'
 
 
-class UserProfileView(generic.DetailView):
-
-	model = User
-	context_object_name = 'profile'
-	template_name = 'profile_detail.html'
-
-
 class ForumCreateReplyView(generic.CreateView):
 
 	model = Post
@@ -46,7 +39,42 @@ class ForumCreateReplyView(generic.CreateView):
 		thread = get_object_or_404(Thread, slug=self.kwargs['slug'])
 		form.instance.thread = thread
 		form.instance.user = self.request.user
-		return super(ForumCreateReplyView, self).form_valid(form)
+		return super().form_valid(form)
+
+
+class ForumPostEditView(generic.UpdateView):
+
+	model = Post
+	context_object_name = 'post'
+	template_name = 'edit_post.html'
+	fields = ['content']
+
+	def form_valid(self, form):
+		this_user = self.request.user.id
+		post_author = self.object.user.id
+
+		if this_user is not post_author:
+			return HttpResponse('Access Denied.')
+		return super().form_valid(form)
+
+
+class ForumPostDeleteView(generic.DeleteView):
+
+	model = Post
+	template_name = 'delete_post.html'
+
+	def delete(self, *args, **kwargs):
+		self.obj = self.get_object()
+		this_user = self.request.user.id
+		post_author = self.obj.user.id
+
+		if this_user is not post_author:
+			return HttpResponse('Access Denied.')
+		return super().delete(self.obj)
+
+	def get_success_url(self):
+		obj = self.get_object()
+		return reverse('forum:view_thread', kwargs={'slug': obj.thread.slug })
 
 
 def ForumCreateThreadView(request):
@@ -79,8 +107,25 @@ def ForumCreateThreadView(request):
 			'create_post_form': create_post_form})
 
 
+class UserProfileView(generic.DetailView):
+
+	model = User
+	context_object_name = 'profile'
+	template_name = 'profile_detail.html'
 
 
+class UserProfileEditView(generic.UpdateView):
+
+	model = User
+	context_object_name = 'profile'
+	template_name = 'edit_profile.html'
+	fields = ['first_name', 'last_name', 'email']
+
+	def get_object(self):
+		return get_object_or_404(User, pk=self.request.user.id)
+
+	def form_valid(self, form):
+		return HttpResponseRedirect(reverse('forum:view_forum_index'))
 
 
 
